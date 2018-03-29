5d7bb0079b8977c79e5cc1ae52075b710cb7647f
helly <helly@email.ccc>
2018-03-29 10:54:14 +0300

    E-mail config

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -2,22 +2,28 @@ from django import forms
 from .models import SignUp
 
 
+class ContactForm(forms.Form):
+    full_name = forms.CharField(required=False)
+    email = forms.EmailField()
+    message = forms.CharField()
+
+
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = ['full_name', 'email']
         # exclude = ['full_name']
 
-        # functions to validating your email
-        def clean_email(self):
-            email = self.cleaned_data.get('email')
-            email_base, provider = email.split("@")
-            domain, extension = provider.split(".")
-            if not extension == "edu":
-                raise forms.ValidationError("Please use a valid .EDU email address")
-            return
+    # functions to validating your email
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+        extension = provider.split(".")[-1]
+        if not extension == "edu":
+            raise forms.ValidationError("Please use a valid .EDU email address")
+        return email
 
-        def clean_full_name(self):
-            full_name = self.cleaned_data.get('full_name')
-            # write validation code.
-            return full_name
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get('full_name')
+        # write validation code.
+        return full_name
